08a3b7eaf9ec71f1798a03bf2f881a03d2597d46
tiggon <tiggon@ip-172-31-33-173.sa-east-1.compute.internal>
2023-04-12 13:56:19 +0000

    first commit

---
+++ constants.js
@@ -0,0 +1,10 @@
+const constants = {
+  videoPublicURL:
+    "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+  videoCodec: "libx264",
+  audioCodec: "aac",
+};
+
+module.exports = {
+  constants,
+};
